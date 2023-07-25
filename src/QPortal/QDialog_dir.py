02a88122e114c444d552ccab6b65cf9e667a8ffe
Luis Alfonso Olivares Jiménez <alfonso.cucei.udg@gmail.com>
2023-07-24 19:45:44 -0700

    Update QDialog_dir.py
    An automatically management of *.dcm files.

--- src/QPortal/QDialog_dir.py
+++ src/QPortal/QDialog_dir.py
@@ -1,5 +1,8 @@
 import sys
+from pathlib import Path
 from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
+
+from pylinac import FieldAnalysis, Centering
 
 
 """QDialog main function."""
@@ -9,7 +12,13 @@ app = QApplication(sys.argv)
 win = QWidget()
 
 dir = QFileDialog.getExistingDirectory(caption = "Open Directory", dir="/home")
-print(dir)
+# dir is a class str
+files = list(Path(dir).glob("RI*.dcm"))
+for file in files:
+    my_img = FieldAnalysis(path = f"{file}")
+    #my_img.analyze(centering = Centering.GEOMETRIC_CENTER)
+    #results = my_img.results_data()
+    print(f"SID: {my_img.image.metadata['RTImageSID'].value}")
 
 win.show()
 # Run the event loop
